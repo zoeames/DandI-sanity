e815cd0080adf4ea3f70dcd948a792ec5eae7d17
Zoe Ames <zoeames@gmail.com>
2021-05-24 00:37:51 -0500

    added main schemas

---
+++ schemas/tag.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHashtag } from '@fortawesome/pro-solid-svg-icons';
+
+export default {
+  name: 'tag',
+  title: 'Tags',
+  type: 'document',
+  icon: () => <FontAwesomeIcon icon={faHashtag} />,
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      description:  'Tag Name'
+    }
+  ]
+}
