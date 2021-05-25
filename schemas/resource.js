8198bee1f22b5a278201c703b774a1f1ee9a56b4
Zoe Ames <zoeames@gmail.com>
2021-05-25 00:08:54 -0500

    resource complete

--- schemas/resource.js
+++ schemas/resource.js
@@ -13,6 +13,74 @@ export default {
       title: 'Title',
       type: 'string',
       description:  'Resource Title'
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'string',
+      description:  'Resource description'
+    },
+    {
+      name: 'author',
+      title: 'Author(s)',
+      type: 'string',
+      description:  'Resource Author(s) - to add multiple separate with commas (John Smith, Jane Doe)'
+    },
+    {
+      name: 'url',
+      title: 'Url',
+      type: 'string',
+      description:  'Resource Url'
+    },
+    {
+      name: 'isActive',
+      title: 'Is the resource active?',
+      description:  'If checked the resource will show on the site.  If unchecked it will not show',
+      type: 'boolean',
+      initialValue: false,
+      options: {
+        layout: 'checkbox',
+      },
+    },
+    {
+      name: 'isFeatured',
+      title: 'Is the resource featured?',
+      description:  'If checked the resource will show in the featured section',
+      type: 'boolean',
+      initialValue: false,
+      options: {
+        layout: 'checkbox',
+      },
+    },
+    {
+      name: 'mediaType',
+      title: 'Media Type',
+      type: 'reference',
+      to: [{ type: 'media' }],
+    },
+    {
+      name: 'topics',
+      title: 'Topics',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'topic' }] }],
+    },
+    {
+      name: 'tags',
+      title: 'Tags',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'tag' }] }],
     }
-  ]
+  ],
+  preview: {
+    select: {
+      title: 'title',
+      isActive: 'isActive',
+    },
+    prepare: ({ title, isActive }) => {
+      return {
+        title,
+        subtitle: `Active: ${isActive ? 'Yes' : 'No'}`,
+      };
+    },
+  },
 }
